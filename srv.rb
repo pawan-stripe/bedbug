4119d986fc5ae6a0b389c7ccaf4f8ca7c59e6c5a
pawan-stripe <150579399+pawan-stripe@users.noreply.github.com>
2024-10-15 09:16:59 +0530

    Create srv.rb

---
+++ srv.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/ruby
+require File.join(File.dirname(__FILE__), 'bedbug')
+# using sinatra for handling web req 
+
+require 'sinatra'
+
+set :host, '127.0.0.1'
+set :port, 1223
+
+get '/' do
+  @bugs = Bug.all
+  @baseurl = $config['baseurl']
+  erb :index
+end
+
+get '/bug/:id' do
+  @bug = Bug.from_id(params[:id])
+  erb :bug
+end
+
+post '/receive-commit' do
+  push = JSON.parse(params[:payload])
+  push['commits'].each do |commit|
+    
+    b = nil
+    if commit['message'] =~ /#(\d+)/
+      b = Bug.from_id($1)
+    elsif commit['message'] =~ /#(\w+)/
+      b = Bug.from_tag($1)
+    end
+
+    if b
+      b.status = 'fixed'
+      b.fixed_in = commit['id']
+      b.save
+    end
+  end
+  'ok'
+end
